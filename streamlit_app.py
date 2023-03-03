52bb7822b751baf24a9e2f10cbc709a816b95006
saurabh2406 <75164936+saurabh2406@users.noreply.github.com>
2023-03-03 13:58:35 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Oatmeal')
+streamlit.text('Spinach')
+streamlit.text('Eggs')
